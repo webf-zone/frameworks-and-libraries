a347547ecf86d9bdc35ce647c3979c13844615d9
Kumar Bhot <kumarbhot@yahoo.com>
2017-12-30 15:25:56 +0530

    Elm: Messages

--- elm/App.elm
+++ elm/App.elm
@@ -1,37 +1,71 @@
 module App exposing (..)
 
-import Html exposing (Html, div, text, program)
+import Html exposing (Html, button, div, text, program)
+import Html.Events exposing (onClick)
+
+
+-- MODEL
 
 
 type alias Model =
-    String
-
-
-type Msg
-    = NoOp
+    Bool
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( "Elm Architecture", Cmd.none )
+    ( False, Cmd.none )
+
+
+
+-- MESSAGES
+
+
+type Msg
+    = Expand
+    | Collapse
+
+
+
+-- VIEW
 
 
 view : Model -> Html Msg
 view model =
-    div []
-        [ text model ]
+    if model then
+        div []
+            [ button [ onClick Collapse ] [ text "Collapse" ]
+            , text "Widget"
+            ]
+    else
+        div []
+            [ button [ onClick Expand ] [ text "Expand" ] ]
+
+
+
+-- UPDATE
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        NoOp ->
-            ( model, Cmd.none )
+        Expand ->
+            ( True, Cmd.none )
+
+        Collapse ->
+            ( False, Cmd.none )
+
+
+
+-- SUBSCRIPTIONS
 
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
     Sub.none
+
+
+
+-- MAIN
 
 
 main : Program Never Model Msg
